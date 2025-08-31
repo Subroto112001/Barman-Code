5467b2fe03210d185542fc6a1979b2f8411f6695
Subroto112001 <subrotokumarbarman@gmail.com>
2025-09-01 01:13:04 +0600

    update

--- src/Component/HomepageComponent/Approach.jsx
+++ src/Component/HomepageComponent/Approach.jsx
@@ -1,27 +1,44 @@
 import React from 'react'
+import { FaCheck, FaCode } from 'react-icons/fa';
+import { GiTechnoHeart } from 'react-icons/gi';
+import { GrSecure, GrShieldSecurity } from 'react-icons/gr';
 import { IoRocket } from 'react-icons/io5';
 
 const Approach = () => {
+   
     const itemall = [
-        {
-            id:1
-        },
-        {
-            id:2
-        },
-        {
-            id:3
-        },
-        {
-            id:4
-        },
-        {
-            id:5
-        },
-        {
-            id:6
-        },
-    ]
+      {
+        id: 1,
+        icon: <IoRocket />,
+        header: "UX Driven Engineering",
+      },
+      {
+        id: 2,
+        icon: <FaCode />,
+        header: "Developing Shared Understanding",
+      },
+      {
+        id: 3,
+        icon: <GiTechnoHeart />,
+        header: "Proven Experience and Expertise",
+      },
+      {
+        id: 4,
+        icon: <GrShieldSecurity />,
+        header: "Security & Intellectual Property (IP)",
+      },
+      {
+        id: 5,
+        icon: <FaCheck />,
+        header: "Code Reviews",
+      },
+      {
+        id: 6,
+        icon: <GrSecure />,
+
+        header: "Quality Assurance & Testing",
+      },
+    ];
     return (
       <div className=" bg-[#f7f7f7]">
         <div className="container py-[70px]">
@@ -33,14 +50,12 @@ const Approach = () => {
             </div>
             <div className="mt-[60px] flex flex-wrap gap-y-[40px] justify-between">
               {itemall.map((item) => (
-                <div className="w-[604px] h-[237px] bg-white px-[34px] py-[60px] border-2 border-blue-300 rounded flex flex-row gap-[25px]">
+                <div className="w-[604px] h-[237px] bg-white px-[34px] py-[60px] border-2 border-blue-300 rounded-lg flex flex-row gap-[25px]">
                   <div className="font-[34px] bg-red-400 text-white w-[40px] h-[40px] flex items-center justify-center rounded">
-                    <IoRocket />
+                    {item.icon}
                   </div>
                   <div className="flex flex-col gap-[10px]">
-                    <h3 className="Headings-h4 text-gray-900">
-                      UX Driven Engineering
-                    </h3>
+                    <h3 className="Headings-h4 text-gray-900">{item.header}</h3>
                     <p className="max-w-[453px] default text-gray-700">
                       Unlike other companies, we are a UX first development
                       company. Projects are driven by designers and they make
